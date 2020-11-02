21351b0f7c4e23ccc55030282069fd34976e8502
danielthoreson <71231857+danielthoreson@users.noreply.github.com>
2020-11-01 20:27:54 -0600

    Project 3 Code Review

--- while.sh
+++ while.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+#All this code does is execute a while loop
+#This while loop will only run one time
+#Raising and or lowering the counter would alter this
 counter=0
 while [ $counter -lt 3 ]; do
 	let counter+=1
